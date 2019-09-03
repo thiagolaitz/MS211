56129dc01e3a379c370e4d216359b28fab148d62
Thiago Soares Laitz <t224898@g.unicamp.br>
2019-09-02 21:09:10 -0300

    Update Representante_turma.m

--- Representante_turma.m
+++ Representante_turma.m
@@ -2,30 +2,32 @@ clear all
 clc
 load DadosProjeto1_TurmaM.mat
 
-
 p = zeros(98,98); %matriz de probabilidade
 q = zeros(98,1); %Matriz coluna para guardar a quantidade de pessoas conhecidas
 alfa = 0.1; %fator de amortecimento
 
-%O primeiro passo é determinar a quantidade de pessoas conhecidas
-
-for j = 98
- for k = 98
- 
+%O primeiro passo é determinar a quantidade de pessoas conhecidas:
+for j = 1:98
+ for k = 1:98
+   
   if (A(j,k) == 1)
-    q(j,1) = q(j,1) + 1 %salva o numero de conhecidos
-  endif
- 
+    q(j,1) = q(j,1) .+ 1; %salva o numero de conhecidos
+  endif 
+  
  endfor
 endfor
 
-q
-%Agora
-%for i = 98
- % 
-  %for j = 98
+%Agora devemos calcular as probabilidades:
+for i = 1:98
+  for j = 1:98
     
+    if (q(i,1) != 0)
+      p(i,j) = A(j,i)/q(i,1);
+    else
+      p(i,j) = 1/98;
+    endif
     
-  %end for
-  
-%endfor
+  endfor
+endfor
+
+%Resolvendo o sistema Linear:
