48c0cf0c14716ddcb3be22b2deff0797b2b0998b
Thiago Soares Laitz <t224898@g.unicamp.br>
2019-09-03 16:25:46 -0300

    Update Representante_turma.m

--- Representante_turma.m
+++ Representante_turma.m
@@ -2,16 +2,18 @@ clear all
 clc
 load DadosProjeto1_TurmaM.mat
 
+d = zeros(98,98); %Matriz dos coeficientes do sistema linear
+b = zeros(98,1); %matriz coluna do sistema linear AX = B
 p = zeros(98,98); %matriz de probabilidade
 q = zeros(98,1); %Matriz coluna para guardar a quantidade de pessoas conhecidas
-alfa = 0.1; %fator de amortecimento
+alfa = 0.85; %fator de amortecimento
 
 %O primeiro passo é determinar a quantidade de pessoas conhecidas:
 for j = 1:98
  for k = 1:98
    
   if (A(j,k) == 1)
-    q(j,1) = q(j,1) .+ 1; %salva o numero de conhecidos
+    q(j,1) = q(j,1) .+ 1; %salva o numero de conhecidos de j
   endif 
   
  endfor
@@ -21,8 +23,8 @@ endfor
 for i = 1:98
   for j = 1:98
     
-    if (q(i,1) != 0)
-      p(i,j) = A(j,i)/q(i,1);
+    if (q(j,1) != 0) %Sendo q(j,1) o numero de pessoas que j conhece
+      p(i,j) = (A(j,i)/q(j,1));
     else
       p(i,j) = 1/98;
     endif
@@ -30,4 +32,24 @@ for i = 1:98
   endfor
 endfor
 
-%Resolvendo o sistema Linear:
+%Ajustando a matriz do sistema Linear:
+
+%Ajustando a matriz B:
+for q = 1:98
+  b(q,1) = (1-alfa)*(1/98);
+endfor
+
+%Calculando a matriz dos coeficientes D:
+for i = 1:98
+  for j = 1:98
+    
+    if (i == j)
+      d(i,j) = p(i,j)*alfa - 1;
+    else
+      d(i,j) = p(i,j)*alfa;
+    endif
+    
+  endfor
+endfor
+b
+d
