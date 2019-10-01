e4bf092b14bb8e52207556b73b28d7808360ecbd
Thiago Soares Laitz <t224898@g.unicamp.br>
2019-10-01 12:37:07 -0300

    Update remove_eco.m

--- remove_eco.m
+++ remove_eco.m
@@ -2,29 +2,25 @@
 
 clc
 
-'''
+
 N = length(y);
 plot([0:N-1]/fs,y);
 axis([0 15 -1 1]);
 grid
-'''
+
 
 %X(i) = y(i) - y(i-1)
 
 A = zeros(120000,1);%matriz com os valores de X
 
 for i = 1:120000
-  
-  if ((i > 8000) && (i < 16000))
-    A(i,1) = y((i-8000),1);
-  endif
-  
+    
   if (i < 8000)
     A(i,1) = y(i,1);
   endif
   
-  if (i > 16000)
-    A(i,1) = y((i-8000),1) - y((i-16000),1);
+  if (i > 8000)
+    A(i,1) = y((i),1) - y((i-8000),1);
   endif 
   
 endfor
